e221c153d129e02690dfb810cbe48f697e955897
Layza Nicolle <layzanicolle1@gmail.com>
2026-05-30 18:35:59 -0300

    Integrando as rotas auth

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-require('./config/db'); 
+require('./config/db'); // Se der erro de módulo aqui, mude para: require('../config/db') ou require('./config/db') dependendo de onde está a pasta config
 
-// 1. IMPORTAR AS ROTAS
 const authRoutes = require('./routes/authRoutes'); 
-const relationRoutes = require('./routes/relationRoutes');
+const authMiddleware = require('./middlewares/auth'); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// 2. ATIVAR AS ROTAS
-app.use(authRoutes); 
-app.use(relationRoutes);
+app.use("/auth", authRoutes); 
 
-// apenas pra testar inicialmente kk
-app.get('/', (req, res) => {
-  res.json({ message: "bem vindo a api do TrackIt!" });
+// Rota de teste para validar o middleware
+app.get("/users", authMiddleware, (req, res) => {
+  res.json({
+    message: "Rota protegida funcionando!",
+    userId: req.userId,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
